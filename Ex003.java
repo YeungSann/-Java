3b0949abc5e2a55527105394bca4c4905518aab7
YeungSann <woodsuop@gmail.com>
2025-08-06 00:53:47 +0900

    Add files via upload
    こちらは演習1-3です。問題は以下の通りです。
    自分の名前を縦向きに（姓と名の間に1行空白を入れて）一文字ずつ表示するプログラムを作成してください。

---
+++ Ex003.java
@@ -0,0 +1,9 @@
+//this program is to print my name in different way.
+public class Ex003{
+  public static void main(String[] args){
+    System.out.println("杨\n\n嘉\n明");
+    System.out.print("杨\n\n嘉\n明\n");
+    System.out.println("杨\n");
+    System.out.println("嘉\n明");
+  }
+}
